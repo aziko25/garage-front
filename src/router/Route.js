0e96296cb40267cd73580bfab8c5aafdcc092e2d
Azizkhuja <azizxoja200425@mail.ru>
2025-01-07 19:33:37 +0500

    fixed router url

--- src/router/Route.js
+++ src/router/Route.js
@@ -1,5 +1,6 @@
 export const APP_ROUTES = {
-  URL: 'https://46dd-84-54-120-83.ngrok-free.app',
+  //URL: 'https://46dd-84-54-120-83.ngrok-free.app',
+  URL: 'https://garage-admin.uz/api',
   LOGIN: '/login',
   DASHBORAD: '/dashboard',
   CARS: '/cars',
