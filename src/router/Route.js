02be2a282b32b9a3f6f236f266d155f501eb310a
Azizkhuja <azizxoja200425@mail.ru>
2025-01-11 15:28:07 +0500

    changed api url

--- src/router/Route.js
+++ src/router/Route.js
@@ -1,6 +1,6 @@
 export const APP_ROUTES = {
-  URL: 'https://c1e9-84-54-66-215.ngrok-free.app',
-  //URL: 'https://garage-admin.uz/api',
+  //URL: 'https://c1e9-84-54-66-215.ngrok-free.app',
+  URL: 'https://garage-admin.uz/api',
   LOGIN: '/login',
   DASHBORAD: '/dashboard',
   CARS: '/cars',
